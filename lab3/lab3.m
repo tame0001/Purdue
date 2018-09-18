2525b9bc3a6fc7c7afa22d1b612874f325717523
tame0001 <thirawat.tam@gmail.com>
2018-09-18 19:12:51 -0400

    show all bean images in separate screen

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -2,16 +2,26 @@ folder = 'C:\Users\tbureete\OneDrive\Purdue\2018\Fall\ABE591\lab3\lab3images';
 filePattern = fullfile(folder, '*.tif');
 srcFiles = dir(filePattern);
 numImages = size(srcFiles,1);
+fontSize = 12;
 
-k = 3; 
-fullFileName = fullfile(srcFiles(k).folder, srcFiles(k).name);
-rgbImage = imread(fullFileName);
-for i = 1 : 3
-    subplot(2, 4, i);
-    imshow(rgbImage(:,:,i))
-    [pixelCount, grayLevels] = imhist(rgbImage(:,:,i));
-    subplot(2, 4, i+4);
-    bar(pixelCount);
-    xlim([0 grayLevels(end)]); % Scale x axis manually.
-    
+for k = 1 : numImages
+    fullFileName = fullfile(srcFiles(k).folder, srcFiles(k).name);
+    rgbImage = imread(fullFileName);
+    redImage = rgbImage(:, :, 1);
+    greenImage = rgbImage(:, :, 2);
+    blueImage = rgbImage(:, :, 3);
+    figure;
+    set(gcf, 'Position', get(0, 'ScreenSize'));
+    subplot(3, 4, 1);
+    imshow(rgbImage);
+    title('RGB Image', 'FontSize', fontSize);
+    subplot(3, 4, 2);
+    imshow(redImage);
+    title('Red Image', 'FontSize', fontSize);
+    subplot(3, 4, 3);
+    imshow(greenImage);
+    title('Green Image', 'FontSize', fontSize);
+    subplot(3, 4, 4);
+    imshow(blueImage);
+    title('Blue Image', 'FontSize', fontSize);
 end
